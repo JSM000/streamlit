69be4dc77b30df74d34a881685ff7f656d488840
JSM000 <dndls321@gmail.com>
2025-05-11 19:36:53 +0900

    feat: 포켓몬 도감

---
+++ practice/variable.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+name = "ㅁㄴㅇ"
+
+st.text("내 이름은 정승민")
+st.text("내 이름은 정승민")
+st.text("내 이름은 정승민")
+
+st.text(f"내 이름은 {name}")
+st.text(f"내 이름은 {name}")
+st.text(f"내 이름은 {name}")
